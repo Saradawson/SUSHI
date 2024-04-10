08ebef728bff31a2172da8dbab60011d74d08da5
unknown <saradawsone@gmail.com>
2024-04-10 01:29:52 -0600

    login keypad ui created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 
 function App() {
+  const btns1 = [1, 2, 3]
+  const btns2 = [4, 5, 6]
+  const btns3 = [7, 8, 9]
+  const btns4 = ['CL', 0, 'GO']
+
+  function btnMaker(btns) {
+    return btns.map((btn) => {
+      return <button style={{ width: '30%', height: '100%', fontSize: '3rem'}}>{btn}</button>
+    })
+  }
+
   return (
     <div className="App" >
       <header>
@@ -11,8 +22,18 @@ function App() {
           SUSHI 
         </h1>
       </header>
-      <div style={{ height: '75%' }}>
-
+      <div id='middle-container'>
+        <div style={{ backgroundColor: '#FFE4E4', height:'80%', width: '30%' }}>
+          <form style={{ height: '100%', width: '100%' }}>
+            <input placeholder='-  -  -  -' style={{ margin: '1rem', textAlign: 'center', fontSize: '2rem'}}></input>
+            <div id='btns-container' style={{ height: '100%', width: '100%' }}>
+              <div classname='btn-row' style={{width: '100%', height: '20%'}}>{btnMaker(btns1)}</div>
+              <div classname='btn-row' style={{width: '100%', height: '20%'}}>{btnMaker(btns2)}</div>
+              <div classname='btn-row' style={{width: '100%', height: '20%'}}>{btnMaker(btns3)}</div>
+              <div classname='btn-row' style={{width: '100%', height: '20%'}}>{btnMaker(btns4)}</div>  
+            </div>
+          </form> 
+        </div>
       </div>
       <footer>
         <h3 style={{ fontSize: '1rem' }}>
